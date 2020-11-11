2e81931d737b28b54099e295020e9bed7050ac63
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-11 19:44:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # complexity.
   hash3=monopoly_with_second_tier()
   hash3[:railroads][:rent_in_dollars]={:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned =>100, :four_pieces_owned => 200}
-  hash3[:railroads][:names]={
+  hash3[:railroads][:names]={:reading_railroad:{},
 end
 
 def monopoly_with_fourth_tier
